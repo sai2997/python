9c9ca34056c707137b822da88f81381853fd728f
sai2997 <sai671316@gmail.com>
2025-06-30 16:43:02 +0530

    Create grid.py

---
+++ grid.py
@@ -0,0 +1,8 @@
+print("Name:A sai ganesh")
+print("USN: 1AY24AI001")
+print("Section: M")
+grid_size = int(input("Enter the size of the grid (for example 6 for 6X6: "))
+for i in range(grid_size):
+    for j in range(grid_size):
+        print("*", end=" ")
+    print()
