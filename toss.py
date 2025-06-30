404f577003ed7eaa941ee4234fafd0d0d7af09e2
sai2997 <sai671316@gmail.com>
2025-06-30 16:41:17 +0530

    Create toss.py

---
+++ toss.py
@@ -0,0 +1,33 @@
+print('Name:A sai ganesh\nUSN: 1AY24AI001 \nSection: M\n ')
+import random
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input().lower()
+toss = random.randint(0, 1)  
+toss_str = 'heads' if toss == 1 else 'tails'
+if guess == toss_str:
+    print('You got it!')
+else:
+    print('Nope! Guess again:')
+    guess = input().lower()
+    if guess == toss_str:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
+import random
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input().lower()
+toss = random.randint(0, 1)  
+toss_str = 'heads' if toss == 1 else 'tails'
+if guess == toss_str:
+    print('You got it!')
+else:
+    print('Nope! Guess again:')
+    guess = input().lower()
+    if guess == toss_str:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
