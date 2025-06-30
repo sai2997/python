513f0a9df6a6f5bad283e966fad093ed5183bb11
sai2997 <sai671316@gmail.com>
2025-06-30 16:51:54 +0530

    Create i range.py

---
+++ i range.py
@@ -0,0 +1,6 @@
+print("Name:A sai ganesh")
+print("USN: 1AY24AI001")
+print("Section: M")
+number = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(f"{number} x {i} = {number * i}")
